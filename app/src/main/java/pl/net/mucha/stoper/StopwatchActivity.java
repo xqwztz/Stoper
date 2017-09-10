d741eebc523e2088c8cd7ac43732bda2c42fdfa2
Wojciech Mucha <wm@hxs.pl>
2017-09-10 09:33:44 +0200

    start, stop, reset

--- app/src/main/java/pl/net/mucha/stoper/StopwatchActivity.java
+++ app/src/main/java/pl/net/mucha/stoper/StopwatchActivity.java
@@ -2,12 +2,29 @@ package pl.net.mucha.stoper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StopwatchActivity extends AppCompatActivity {
+    private int seconds = 0;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
     }
+
+    public void onClickStart(View view){
+        running = true;
+    }
+
+    public void onClickStop(View view){
+        running = false;
+    }
+
+    public void onClickReset(View view){
+        seconds = 0;
+        running = false;
+    }
 }
+
