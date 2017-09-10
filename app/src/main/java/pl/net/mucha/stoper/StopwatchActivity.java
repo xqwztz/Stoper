a8cd0a596289219d126c125aeeae74bc36284059
Wojciech Mucha <wm@hxs.pl>
2017-09-10 09:52:44 +0200

    add hanndler to count time

--- app/src/main/java/pl/net/mucha/stoper/StopwatchActivity.java
+++ app/src/main/java/pl/net/mucha/stoper/StopwatchActivity.java
@@ -1,8 +1,10 @@
 package pl.net.mucha.stoper;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class StopwatchActivity extends AppCompatActivity {
     private int seconds = 0;
@@ -12,6 +14,7 @@ public class StopwatchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        runTimer();
     }
 
     public void onClickStart(View view){
@@ -25,6 +28,27 @@ public class StopwatchActivity extends AppCompatActivity {
     public void onClickReset(View view){
         seconds = 0;
         running = false;
+    }
+
+    private void runTimer(){
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run(){
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running){
+                    seconds++;
+                }
+                handler.postDelayed(this,1000);
+            }
+
+        });
+
     }
 }
 
